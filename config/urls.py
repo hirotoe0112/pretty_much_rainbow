7ff1c472e76eec3c0c68518dcec3365bb9e9ab51
hirotoe0112 <49823364+hirotoe0112@users.noreply.github.com>
2024-06-24 17:59:13 +0900

    アプリをappsフォルダに統合

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("index.urls")),
-    path("polls/", include("polls.urls")),
+    path("", include("apps.index.urls")),
+    path("polls/", include("apps.polls.urls")),
     path("admin/", admin.site.urls),
 ]
