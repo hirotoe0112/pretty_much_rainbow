f6c8db1c2dccfa02b093d6d2f7af21e5b6180124
hirotoe0112 <hirotoe0112@gmail.com>
2024-06-25 17:47:49 +0900

    indexページ修正

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,37 +1,22 @@
 from django.shortcuts import render
 from django.template import loader
-import datetime as dt
+import random
 
-function_list = {
-    "2024_06": [
-        {
-            "name": "機能A",
-            "done": True,
-        },
-        {
-            "name": "機能B",
-            "done": False,
-        },
-        {
-            "name": "機能#",
-            "done": False,
-        },
-    ],
-    "2024_07": [],
-}
+fc_list = [
+    {
+        "name": "四面楚歌",
+        "meaning": "四面楚歌，形容处境危险，四面受敌。出自《史记·项羽本纪》。",
+    },
+    {
+        "name": "五里霧中",
+        "meaning": "形容事物模糊不清，不易看清。出自《史记·平原君虞卿列传》。",
+    },
+]
 
 
 # Create your views here.
 def index(request):
-    current_ym = dt.datetime.now().strftime("%Y_%m")
-    functions = function_list[current_ym]
-    # done=Trueのものをカウント
-    done_count = sum(1 for function in functions if function["done"])
-    # 進捗率
-    progress = round(done_count / len(functions) * 100, 1)
     context = {
-        "version": "1.0.0",
-        "progress_name": f"function_{current_ym}",
-        "progress": progress,
+        "fc": random.choice(fc_list),
     }
     return render(request, "index/index.html", context)
