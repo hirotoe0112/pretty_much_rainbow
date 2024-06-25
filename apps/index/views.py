fe1edd7229dccec96802c1d77f64706a248f75ea
hirotoe0112 <hirotoe0112@gmail.com>
2024-06-25 13:53:18 +0900

    index機能追加

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
 from django.template import loader
+import datetime as dt
+
+function_list = {
+    "2024_06": [
+        {
+            "name": "機能A",
+            "done": True,
+        },
+        {
+            "name": "機能B",
+            "done": False,
+        },
+        {
+            "name": "機能#",
+            "done": False,
+        },
+    ],
+    "2024_07": [],
+}
 
 
 # Create your views here.
 def index(request):
+    current_ym = dt.datetime.now().strftime("%Y_%m")
+    functions = function_list[current_ym]
+    # done=Trueのものをカウント
+    done_count = sum(1 for function in functions if function["done"])
+    # 進捗率
+    progress = round(done_count / len(functions) * 100, 1)
     context = {
         "version": "1.0.0",
+        "progress_name": f"function_{current_ym}",
+        "progress": progress,
     }
     return render(request, "index/index.html", context)
